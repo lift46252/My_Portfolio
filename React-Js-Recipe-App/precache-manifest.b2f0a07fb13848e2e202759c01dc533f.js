d5ebbb7feaf0eb71da62b90033d67491b5142545
lift46252 <72867691+lift46252@users.noreply.github.com>
2020-10-23 19:52:53 +0300

    Add files via upload

---
+++ React-Js-Recipe-App/precache-manifest.b2f0a07fb13848e2e202759c01dc533f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cfa0ecb33f5cae15682656c1321dd83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c38acba547847e841c4",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "5132d754b8e62d350f4e",
+    "url": "/static/css/main.8e2159fe.chunk.css"
+  },
+  {
+    "revision": "1c38acba547847e841c4",
+    "url": "/static/js/2.3f564dc2.chunk.js"
+  },
+  {
+    "revision": "78fd0f62364854e39c53711d027f73a3",
+    "url": "/static/js/2.3f564dc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5132d754b8e62d350f4e",
+    "url": "/static/js/main.2e9cac55.chunk.js"
+  },
+  {
+    "revision": "c258e619b438e035fe8f",
+    "url": "/static/js/runtime-main.332e5556.js"
+  }
+]);
